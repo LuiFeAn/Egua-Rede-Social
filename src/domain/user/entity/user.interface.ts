d262f5801c30cd5dfc13db9c9b094e5cf3a14a7f
luifean <luisfernandogvv@gmail.com>
2024-10-02 08:52:17 -0300

    feat: inserting password on user

--- src/domain/user/entity/user.interface.ts
+++ src/domain/user/entity/user.interface.ts
@@ -4,4 +4,5 @@ export default interface IUser {
   email: string;
   age: number;
   nickname: string;
+  password: string;
 }
--- src/domain/user/entity/user.ts
+++ src/domain/user/entity/user.ts
@@ -9,10 +9,12 @@ export class User extends RootEntity {
   private _nickname: string;
   private _following: number = 0;
   private _followers: number = 0;
+  private _password: string;
 
-  constructor({ id, username, email, age, nickname }: IUser) {
+  constructor({ id, username, email, age, nickname, password }: IUser) {
     super(id);
     this._username = username;
+    this._password = password;
     this._email = email;
     this._age = age;
     this._nickname = nickname;
@@ -37,6 +39,10 @@ export class User extends RootEntity {
 
   get nickname() {
     return this._nickname;
+  }
+
+  get password() {
+    return this._password;
   }
 
   get following() {
