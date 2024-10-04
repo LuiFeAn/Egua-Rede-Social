3b39fe7162b2817193c9022a762fdcfd706b9f6b
luifean <luisfernandogvv@gmail.com>
2024-10-04 14:33:45 -0300

    fix: notification instance

--- src/application/@shared/use-case/root.use-case.ts
+++ src/application/@shared/use-case/root.use-case.ts
@@ -1,6 +1,6 @@
 import Notification from '@domain/@shared/notification';
 export default abstract class RootUseCase {
-  private _notification: Notification;
+  private _notification: Notification = new Notification()
 
   get notification() {
     return this._notification;
