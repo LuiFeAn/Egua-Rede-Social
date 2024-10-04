0ccd40ca05b1e830d142e693145ab72438efbf33
luifean <luisfernandogvv@gmail.com>
2024-10-04 10:22:22 -0300

    tests: test adjusts

---
+++ src/domain/core/egua/follower/entity/follower.unit.spec.ts
@@ -0,0 +1,125 @@
+import { randomUUID } from 'crypto';
+import { Follower } from './follower';
+import { Following } from '../../following/entity/following';
+import { Password } from '../../user/entity/value_object/password/password';
+describe('FollowerEntity Unit Tests', () => {
+  it('Should throw a error if user already follow a user', () => {
+    const input1 = {
+      id: randomUUID(),
+      username: 'Luis Fernando',
+      password: new Password('Str0ngP@ssword12'),
+      email: 'testexxx@email.com',
+      age: 24,
+      nickname: 'LuiFeAn',
+    };
+
+    const input2 = {
+      id: randomUUID(),
+      username: 'Uncle Bob',
+      password: new Password('Str0ngP@ssword12'),
+      email: 'testexxx@email.com',
+      age: 24,
+      nickname: 'LuiFeAn',
+    };
+
+    const following = new Following(input2);
+
+    const follower = new Follower(input1);
+
+    follower.follow(following.id);
+
+    expect(() => {
+      follower.follow(following.id);
+    }).toThrow('Already following this user');
+  });
+
+  it('Should follow a user', () => {
+    const input1 = {
+      id: randomUUID(),
+      username: 'Luis Fernando',
+      password: new Password('Str0ngP@ssword12'),
+      email: 'testexxx@email.com',
+      age: 24,
+      nickname: 'LuiFeAn',
+    };
+
+    const input2 = {
+      id: randomUUID(),
+      username: 'Uncle Bob',
+      password: new Password('Str0ngP@ssword12'),
+      email: 'testexxx@email.com',
+      age: 24,
+      nickname: 'LuiFeAn',
+    };
+
+    const input3 = {
+      id: randomUUID(),
+      username: 'Uncle Bob',
+      password: new Password('Str0ngP@ssword12'),
+      email: 'testexxx@email.com',
+      age: 24,
+      nickname: 'LuiFeAn',
+    };
+
+    const following1 = new Following(input2);
+    const following2 = new Following(input3);
+
+    const follower = new Follower(input1);
+
+    follower.follow(following1.id);
+    follower.follow(following2.id);
+
+    expect(follower.followingCount).toBe(2);
+    expect(follower.following).toEqual([following1.id, following2.id]);
+  });
+
+  it('Should unfollow a user', () => {
+    const input1 = {
+      id: randomUUID(),
+      username: 'Luis Fernando',
+      password: new Password('Str0ngP@ssword12'),
+      email: 'testexxx@email.com',
+      age: 24,
+      nickname: 'LuiFeAn',
+    };
+
+    const input2 = {
+      id: randomUUID(),
+      username: 'Uncle Bob',
+      password: new Password('Str0ngP@ssword12'),
+      email: 'testexxx@email.com',
+      age: 24,
+      nickname: 'LuiFeAn',
+    };
+
+    const input3 = {
+      id: randomUUID(),
+      username: 'Uncle Bob',
+      password: new Password('Str0ngP@ssword12'),
+      email: 'testexxx@email.com',
+      age: 24,
+      nickname: 'LuiFeAn',
+    };
+
+    const following1 = new Following(input2);
+    const following2 = new Following(input3);
+
+    const follower = new Follower(input1);
+
+    follower.follow(following1.id);
+    follower.follow(following2.id);
+
+    expect(follower.followingCount).toBe(2);
+    expect(follower.following).toEqual([following1.id, following2.id]);
+
+    follower.unfollow(following1.id);
+
+    expect(follower.followingCount).toBe(1);
+    expect(follower.following).toEqual([following2.id]);
+
+    follower.unfollow(following2.id);
+
+    expect(follower.followingCount).toBe(0);
+    expect(follower.following).toEqual([]);
+  });
+});
---
+++ src/domain/core/egua/following/entity/following.unit.spec.ts
@@ -0,0 +1,79 @@
+import { randomUUID } from 'crypto';
+import { Following } from '../../following/entity/following';
+import { Password } from '../../user/entity/value_object/password/password';
+describe('FollowerEntity Unit Tests', () => {
+  it('Should throw a error if user already a follower', () => {
+    const input1 = {
+      id: randomUUID(),
+      username: 'Luis Fernando',
+      password: new Password('Str0ngP@ssword12'),
+      email: 'testexxx@email.com',
+      age: 24,
+      nickname: 'LuiFeAn',
+    };
+
+    const followerUuid1 = randomUUID();
+
+    const following = new Following(input1);
+
+    following.addFollower(followerUuid1);
+
+    expect(() => {
+      following.addFollower(followerUuid1);
+    }).toThrow('This user is already a follower');
+  });
+
+  it('Should add a new follower', () => {
+    const input1 = {
+      id: randomUUID(),
+      username: 'Luis Fernando',
+      password: new Password('Str0ngP@ssword12'),
+      email: 'testexxx@email.com',
+      age: 24,
+      nickname: 'LuiFeAn',
+    };
+
+    const followerUuid1 = randomUUID();
+    const followerUuid2 = randomUUID();
+
+    const following = new Following(input1);
+
+    following.addFollower(followerUuid1);
+    following.addFollower(followerUuid2);
+
+    expect(following.followerCount).toBe(2);
+    expect(following.followers).toEqual([followerUuid1, followerUuid2]);
+  });
+
+  it('Should remove a follower', () => {
+    const input1 = {
+      id: randomUUID(),
+      username: 'Luis Fernando',
+      password: new Password('Str0ngP@ssword12'),
+      email: 'testexxx@email.com',
+      age: 24,
+      nickname: 'LuiFeAn',
+    };
+
+    const followerUuid1 = randomUUID();
+    const followerUuid2 = randomUUID();
+
+    const following = new Following(input1);
+
+    following.addFollower(followerUuid1);
+    following.addFollower(followerUuid2);
+
+    expect(following.followerCount).toBe(2);
+    expect(following.followers).toEqual([followerUuid1, followerUuid2]);
+
+    following.removeFollower(followerUuid1);
+
+    expect(following.followerCount).toBe(1);
+    expect(following.followers).toEqual([followerUuid2]);
+
+    following.removeFollower(followerUuid2);
+
+    expect(following.followerCount).toBe(0);
+    expect(following.followers).toEqual([]);
+  });
+});
